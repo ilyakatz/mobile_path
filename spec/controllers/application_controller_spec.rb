774ffca788acdaa7b0a63cd4ac3f65e51520a83c
Ilya Katz <ilyakatz@gmail.com>
2011-09-04 16:00:31 -0400

    added tests that views actually contain mobile views

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,9 +1,12 @@
 require 'spec_helper'
 
 describe ApplicationController do
+
+  render_views
+
   controller do
     def index
-      render :nothing => true
+      render :text => "blah"
     end
   end
 
@@ -12,6 +15,7 @@ describe ApplicationController do
       request.env["HTTP_USER_AGENT"] = "Mozilla/5.0 (iPhone; U; XXXXX like Mac OS X; en) AppleWebKit/420+ (KHTML, like Gecko) Version/3.0 Mobile/241 Safari/419.3 |"
       get :index
       response.location.split("http://")[1].split(".")[0].should eq(MobilePath.config.subdomain)
+      response.code.should == "302"
     end
 
     it "should redirect to a configured domain as well." do
@@ -21,28 +25,33 @@ describe ApplicationController do
       end
       get :index
       response.location.split("http://")[1].split(".")[0].should eq("mobizzle")
+      response.code.should == "302"
     end
 
     it "shouldn't redirect on a normal user agent" do
       request.env["HTTP_USER_AGENT"] = "Test"
       get :index
       response.location.should be_false
+      controller.view_paths.select{|view|view.to_s=~/#{MobilePath.config.mobile_view_path}/}.should_not be_present
     end
 
     it "shouldn't redirect on a mobile user agent with a full_site param" do
       request.env["HTTP_USER_AGENT"] = "Mozilla/5.0 (iPhone; U; XXXXX like Mac OS X; en) AppleWebKit/420+ (KHTML, like Gecko) Version/3.0 Mobile/241 Safari/419.3 |"
       get :index, {:full_site => true}
+      controller.view_paths.select{|view|view.to_s=~/#{MobilePath.config.mobile_view_path}/}.should be_present
       response.location.should be_false
 
       get :index, {:mobile_site => true}
+      controller.view_paths.select{|view|view.to_s=~/#{MobilePath.config.mobile_view_path}/}.should be_present
       response.location.split("http://")[1].split(".")[0].should eq(MobilePath.config.subdomain)
     end
 
     it "should not fail if mobile subdomain is empty" do
       MobilePath.config.subdomain=""
       request.env["HTTP_USER_AGENT"] = "Mozilla/5.0 (iPhone; U; XXXXX like Mac OS X; en) AppleWebKit/420+ (KHTML, like Gecko) Version/3.0 Mobile/241 Safari/419.3 |"
-      get :index, {:mobile_site => true}
-      response.location.should eq("http://test.host/stub_resources?mobile_site=true")
+      get :index
+      response.location.should be_nil
+      controller.view_paths.select{|view|view.to_s=~/#{MobilePath.config.mobile_view_path}/}.should be_present
     end
 
   end
