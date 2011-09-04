0337e876cc3d3be190e6a2eec1bd34a486be6f22
Ilya Katz <ilyakatz@gmail.com>
2011-09-04 13:39:10 -0400

    borrowed mobile_fu regex for mobile devises

--- lib/mobile_path/configuration.rb
+++ lib/mobile_path/configuration.rb
@@ -25,7 +25,12 @@ module MobilePath
     @@defaults = {
       :subdomain => 'm',
       :mobile_view_path => "mobile_views",
-      :mobile_device_regex => /(iPhone|iPod|iPad|Android)/
+      :mobile_device_regex => /('palm|blackberry|nokia|phone|midp|mobi|symbian|chtml|ericsson|minimo|' +
+                          'audiovox|motorola|samsung|telit|upg1|windows ce|ucweb|astel|plucker|' +
+                          'x320|x240|j2me|sgh|portable|sprint|docomo|kddi|softbank|android|mmp|' +
+                          'pdxgw|netfront|xiino|vodafone|portalmmm|sagem|mot-|sie-|ipod|up\\.b|' +
+                          'webos|amoi|novarra|cdm|alcatel|pocket|ipad|iphone|mobileexplorer|' +
+                          'mobile')/i
     }
     
     def self.defaults
